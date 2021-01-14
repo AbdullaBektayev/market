2c083ef789c58274cec27af3d6595868715aab43
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-01-14 20:24:28 +0600

    01.13.2021 seventeenth commit added docker + celery with saved data

--- market_place/market_place/settings.py
+++ market_place/market_place/settings.py
@@ -164,7 +164,7 @@ CELERY_TIME_ZONE: 'UTC'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/',
+        'LOCATION': 'redis://redis:6379/',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
